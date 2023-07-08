c8c2df5ba6761d484418e0d641dc410fbd5e4dbf
Githaidar <hilmiyatulasna63@gmail.com>
2023-07-08 14:26:01 +0700

    copyright

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 const Copyright = () => {
-  return (
-    <div className='border-t border-white/20'>
-    <div className='container mx-auto'>
-      <div className='py-6'>
-        <p className='text-base text-center'>Copyright &copy; 2023 Bistro Cafe. All right reserved.</p>
-        </div>
-        </div>
-  </div>
-  );
+    return ( <
+        div className = 'border-t border-white/20' >
+        <
+        div className = 'container mx-auto' >
+        <
+        div className = 'py-6' >
+        <
+        p className = 'text-base text-center' > Copyright & copy; 2023 KulinerBogor.com All right reserved. < /p> <
+        /div> <
+        /div> <
+        /div>
+    );
 };
 
 export default Copyright;
